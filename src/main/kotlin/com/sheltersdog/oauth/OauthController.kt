db224bb01c8c09ef17d5c83fa638f34a6b6d3edf
mysend12 <mysend12@naver.com>
2023-08-05 19:24:42 +0900

    refactor: 의미없는 주석 제거

--- src/main/kotlin/com/sheltersdog/oauth/OauthController.kt
+++ src/main/kotlin/com/sheltersdog/oauth/OauthController.kt
@@ -6,7 +6,6 @@ import com.sheltersdog.oauth.dto.KakaoUserInfoDto
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
-import reactor.core.publisher.Mono
 
 @RestController
 @RequestMapping("/oauth")
