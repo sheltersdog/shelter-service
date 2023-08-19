2aeb4db204f343286c7b6ab4f0f168018d592715
mysend12 <mysend12@naver.com>
2023-08-19 16:32:13 +0900

    fix: #8 예외 발생시 사용할 예외 타입 추가

---
+++ src/main/kotlin/com/sheltersdog/core/exception/ExceptionType.kt
@@ -0,0 +1,41 @@
+package com.sheltersdog.core.exception
+
+import com.sheltersdog.core.log.LogMessage
+
+enum class ExceptionType {
+    NOT_FOUND_ADDRESS,
+
+    NOT_FOUND_KAKAO_DOCUMENT,
+    NOT_FOUND_SHELTER,
+    VALID_CHECK_WRONG,
+
+    ACCESS_TOKEN_WRONG,
+    ACCESS_DENIED,
+
+    NOT_FOUND_FOREVERDOG,
+    DB_UPDATE_FAIL,
+
+    NOT_FOUND_KAKAO_TOKEN,
+    NOT_FOUND_KAKAO_USER_INFO,
+
+    FILE_TYPE_WRONG,
+
+    KAKAO_LEAVE_MESSAGE,
+
+    ALREADY_JOIN_USER,
+    NOT_FOUND_USER,
+
+    VOLUNTEER_NOT_FOUND,
+}
+
+fun ExceptionType.logMessage(): LogMessage {
+    return LogMessage.values().first { logMessage ->
+        logMessage.name == this.name
+    }
+}
+
+fun ExceptionType.exceptionMessage(): ExceptionMessage {
+    return ExceptionMessage.values().first { exceptionMessage ->
+        exceptionMessage.name == this.name
+    }
+}
