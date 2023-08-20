d4bb11201806cd07ca33fcf50fd20e3f1330be36
mysend12 <mysend12@naver.com>
2023-08-20 15:16:31 +0900

    refactor: hasAuthority 함수를 더 쉽게 사용할 수 있도록 확장 (Shelter, List<ShelterJoinUser>의 확장함수로 추가)

--- src/main/kotlin/com/sheltersdog/shelter/util/ShelterAuthorityChecker.kt
+++ src/main/kotlin/com/sheltersdog/shelter/util/ShelterAuthorityChecker.kt
@@ -1,11 +1,49 @@
 package com.sheltersdog.shelter.util
 
+import com.sheltersdog.core.exception.ExceptionType
+import com.sheltersdog.core.exception.SheltersdogException
+import com.sheltersdog.shelter.entity.Shelter
 import com.sheltersdog.shelter.entity.ShelterJoinUser
 import com.sheltersdog.shelter.entity.model.ShelterAuthority
 import kotlinx.coroutines.reactive.awaitSingle
 import org.springframework.security.core.context.ReactiveSecurityContextHolder
 import org.springframework.security.core.userdetails.User
 
+suspend fun Shelter.hasAuthorityOrThrow(
+    userId: String? = null,
+    shelterAuthorities: List<ShelterAuthority>,
+) {
+    this.sheltersAdmins.hasAuthorityOrThrow(
+        shelterId = this.id.toString(),
+        userId = userId,
+        shelterAuthorities = shelterAuthorities,
+    )
+}
+
+suspend fun List<ShelterJoinUser>.hasAuthorityOrThrow(
+    shelterId: String,
+    userId: String? = null,
+    shelterAuthorities: List<ShelterAuthority>,
+) {
+    val hasAuthority = this.hasAuthority(
+        userId = userId,
+        shelterAuthorities = shelterAuthorities,
+    )
+
+    if (hasAuthority) return
+
+    val checkUserId = userId
+        ?: (ReactiveSecurityContextHolder.getContext().awaitSingle().authentication.principal as User).username
+
+    throw SheltersdogException(
+        exceptionType = ExceptionType.ACCESS_DENIED,
+        variables = mapOf(
+            "userId" to checkUserId,
+            "shelterId" to shelterId,
+            "shelterAuthority" to shelterAuthorities
+        )
+    )
+}
 
 suspend fun List<ShelterJoinUser>.hasAuthority(
     userId: String? = null,
